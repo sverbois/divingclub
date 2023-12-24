78e4f53002d6faf6b29c5b7cccce629ad01345d0
Sébastien Verbois <sebastien.verbois@imio.be>
2023-12-24 10:41:40 +0100

    Add member-information view

---
+++ src/divingclub/views/memberinformation.py
@@ -0,0 +1,34 @@
+import datetime
+import json
+
+from plone import api
+from plone.dexterity.browser.view import DefaultView
+from plone.memoize.view import memoize
+from zope.component import getUtility
+from zope.schema.interfaces import IVocabularyFactory
+
+
+class MemberInformationView(DefaultView):
+    @property
+    @memoize
+    def categories(self):
+        factory = getUtility(IVocabularyFactory, "divingclub.DiverCategories")
+        infos = {term.value: term.title for term in factory(None)}
+        return infos
+
+    @property
+    def member_information(self):
+        categories = self.categories
+        users = api.user.get_users()
+        infos = []
+        for u in users:
+            user_infos = {
+                "lastname": u.getProperty("lastname"),
+                "firstname": u.getProperty("firstname"),
+                "email": u.getProperty("email"),
+                "phone": u.getProperty("phone"),
+                "diver_category": categories.get(u.getProperty("diver_category"), None),
+            }
+            infos.append(user_infos)
+
+        return infos

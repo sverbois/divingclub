5efb71e037877eae0985bd3f023d1c9ecbf0d645
Sébastien Verbois <sebastien.verbois@imio.be>
2024-01-22 14:10:25 +0100

    Add Reviewer local role for trip manager

---
+++ src/divingclub/contents/trip/adapters.py
@@ -0,0 +1,23 @@
+from borg.localrole.interfaces import ILocalRoleProvider
+from zope.component import adapter
+from zope.interface import implementer
+
+from .content import ITrip
+
+
+@adapter(ITrip)
+@implementer(ILocalRoleProvider)
+class LocalRoleAdapter(object):
+    def __init__(self, context):
+        self.context = context
+
+    def getRoles(self, principal):
+        """Grant permission for principal"""
+        roles = []
+        if principal == self.context.manager:
+            roles.append("Reviewer")
+        return roles
+
+    def getAllRoles(self):
+        """Grant permissions"""
+        return [(self.context.manager, ("Reviewer",))]
